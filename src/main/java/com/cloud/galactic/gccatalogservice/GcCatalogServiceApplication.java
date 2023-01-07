04fc8657d59ed21b7f5af3415f5624a38af60ccb
datnguyen <39542121+vietdat0308@users.noreply.github.com>
2023-01-07 11:52:45 +0700

    [OnePos-Catalog] init project

---
+++ src/main/java/com/cloud/galactic/gccatalogservice/GcCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.galactic.gccatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/galactic/gccatalogservice/GcCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.galactic.gccatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
