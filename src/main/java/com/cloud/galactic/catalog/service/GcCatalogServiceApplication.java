6e50d83fafef3a398548f61eb330945a86d9b04d
datnguyen <39542121+vietdat0308@users.noreply.github.com>
2023-02-18 01:32:04 +0700

    [OnePos-Catalog] Configure OpenAPI Server

--- src/main/java/com/cloud/galactic/catalog/service/GcCatalogServiceApplication.java
+++ src/main/java/com/cloud/galactic/catalog/service/GcCatalogServiceApplication.java
@@ -1,13 +1,22 @@
 package com.cloud.galactic.catalog.service;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GcCatalogServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GcCatalogServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GcCatalogServiceApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addServersItem(new Server().url("https://api.onepos.com.vn/catalog-service/api/v2"));
+    }
 
 }
